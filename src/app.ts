9bbb562979744b11286f1dbc025156ba9db2867d
Alina Vagner <ali.vag977@gmail.com>
2024-10-09 16:46:44 +0300

    fix comments

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,4 @@
 import express from "express";
-// import express from "express";
 import mongoose from "mongoose";
 import users from "./routes/users";
 import cards from "./routes/cards";
--- src/routes/users.ts
+++ src/routes/users.ts
@@ -3,15 +3,15 @@ import {
   getUsers,
   getUserById,
   postUser,
-  // patchMe,
-  // patchMyAvatar,
+  patchMe,
+  patchMyAvatar,
 } from "../controllers/users";
 
 const router = Router();
 router.get("/users", getUsers);
 router.get("/users/:userId", getUserById);
 router.post("/users", postUser);
-// router.patch("/users/me", patchMe);
-// router.patch("/users/me/avatar", patchMyAvatar);
+router.patch("/users/me", patchMe);
+router.patch("/users/me/avatar", patchMyAvatar);
 
 export default router;
